6eb36f52bfe82520682927fa0cbe01c66fda0c18
maybe-adamant <maybe.adamant@gmail.com>
2026-04-20 11:48:36 -0400

    Align imgui test harness with runtime enums

--- tests/TestUtils.lua
+++ tests/TestUtils.lua
@@ -70,10 +70,38 @@ rom = {
         SetupRunData = function() end,
     },
     ImGui = {},
+    ImGuiCol = {
+        Text = 1,
+    },
     gui = {
         add_to_menu_bar = function() end,
         add_imgui = function() end,
     },
+}
+
+ImGuiComboFlags = {
+    NoPreview = 64,
+}
+
+ImGuiCol = rom.ImGuiCol
+
+ImGuiTreeNodeFlags = {
+    None = 0,
+    Selected = 1,
+    Framed = 2,
+    AllowOverlap = 4,
+    NoTreePushOnOpen = 8,
+    NoAutoOpenOnLog = 16,
+    DefaultOpen = 32,
+    OpenOnDoubleClick = 64,
+    OpenOnArrow = 128,
+    Leaf = 256,
+    Bullet = 512,
+    FramePadding = 1024,
+    SpanAvailWidth = 2048,
+    SpanFullWidth = 4096,
+    NavLeftJumpsBackHere = 8192,
+    CollapsingHeader = 26,
 }
 
 -- Mock ENVY: auto() returns an empty table, sets up public/envy globals
